f961524aca396856636822ee91d2ebaabb8ea0fa
LektorSpb <now.white@mail.ru>
2015-03-17 19:43:24 +0300

    First commit

---
+++ mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "mainwindow2.h"
+#include "ui_mainwindow.h"
+#include <QApplication>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+  Olg = new MainWindow2(this);
+    Olg->show();
+   // Olg.setModal(true);
+  //  Olg.exec();
+   // connect()
+}
