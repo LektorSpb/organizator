6da544aa0ce90d93bee4637d5ca323e198978fb7
LektorSpb <now.white@mail.ru>
2015-03-26 22:55:57 +0300

    dfh

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 //первый первый раз два...а я вот день рожденье не буду справлять!Все...
+    //dksgjkslfdmg
     return a.exec();
 }
